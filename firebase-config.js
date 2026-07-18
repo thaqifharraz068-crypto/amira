631ee9d9e7e72a8ae2e776d8a4b0b1d9811c49f0
thaqifharraz068-crypto <thaqifharraz068@gmail.com>
2026-07-18 16:01:10 +0800

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Konfigurasi Firebase anda
+const firebaseConfig = {
+  apiKey: "AIzaSyCEYcSQL_vA6ut1gChhvfmtXTpc8pPaMNk",
+  authDomain: "izwan-amira-wedding.firebaseapp.com",
+  projectId: "izwan-amira-wedding",
+  storageBucket: "izwan-amira-wedding.firebasestorage.app",
+  messagingSenderId: "448660935827",
+  appId: "1:448660935827:web:b06d34d7608e8fb3d4e966",
+  measurementId: "G-1BJ0HDR8G7"
+};
+// Mulakan Firebase
+firebase.initializeApp(firebaseConfig);
+const database = firebase.databate();
